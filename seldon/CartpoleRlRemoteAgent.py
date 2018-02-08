bf8558018ab8cbab289af6bb0e9df03251f0c81d
David Suarez <david.suarez@intelygenz.com>
2018-02-08 18:18:25 +0100

    feat: GCP Kubernetes

---
+++ seldon/CartpoleRlRemoteAgent.py
@@ -0,0 +1,11 @@
+from keras.models import load_model
+import numpy as np
+
+
+class CartpoleRlRemoteAgent(object):
+
+    def __init__(self):
+        self.model = load_model('Cartpole-rl-remote.h5')
+
+    def predict(self, state):
+        return np.argmax(self.model.predict(state)[0])
