10e68b93c5680569d7d0e4b05804527066c0da76
enrique.garcia.pablos <enrique.garcia.pablos@bbva.com>
2018-02-27 11:55:47 +0100

    Adding venv 2.7

--- seldon/client.py
+++ seldon/client.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 import requests
-from .build.proto import prediction_pb2
-from .build.proto import prediction_pb2_grpc
+from build.proto import prediction_pb2
+from build.proto import prediction_pb2_grpc
 import grpc
 from requests.auth import HTTPBasicAuth
 
-KUBERNETES_IP = "35.226.173.5"
+KUBERNETES_IP = "35.192.141.146"
 
 
 def get_token():
     payload = {'grant_type': 'client_credentials'}
     response = requests.post(
-        "http://" + KUBERNETES_IP + ":8080/oauth/token",
+        "http://{}:8080/oauth/token".format(KUBERNETES_IP),
         auth=HTTPBasicAuth('oauth-key', 'oauth-secret'),
         data=payload)
     token = response.json()["access_token"]
-    print(token)
     return token
 
 
@@ -24,7 +23,7 @@ def rest_request():
     headers = {'Authorization': 'Bearer ' + token}
     payload = {"data": {"names": ["names"], "tensor": {"shape": [1, 4], "values": [0, 0, 1, 1]}}}
     response = requests.post(
-        "http://" + KUBERNETES_IP + ":8080/api/v0.1/predictions",
+        "http://{}:8080/api/v0.1/predictions".format(KUBERNETES_IP),
         headers=headers,
         json=payload)
     print(response.text)
