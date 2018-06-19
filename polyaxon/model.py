cb7fa74798fb49af949fea3fea82902959d13deb
Enrique <engapa@gmail.com>
2018-06-19 15:33:13 +0200

    [skip ci] Polyaxon example

--- polyaxon/model.py
+++ polyaxon/model.py
@@ -1,6 +1,7 @@
+from __future__ import division, print_function, absolute_import
+
 import sys
 
-from __future__ import division, print_function, absolute_import
 
 from cartpole.client.shell_cmd import main as cartpole_main
 from polyaxon_helper import get_outputs_path, send_metrics, get_log_level
